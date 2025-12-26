14e1a6ab9043e8a6648141d05dd72eed55ab15d5
Austin Vu <freeluck.v.i.p999@gmail.com>
2025-12-26 04:25:15 +0000

    updated update script for doas users

--- daily-update-for-gentoo-systems.sh
+++ daily-update-for-gentoo-systems.sh
@@ -8,3 +8,14 @@ sudo dispatch-conf
 sudo emerge --ask --depclean
 sudo eclean -d distfiles
 sudo eclean-pkg
+
+#if you have doas uncomment and delete sudo portion
+
+#doas emaint sync --auto
+#doas emerge --ask --update --newuse --deep --verbose @world
+#doas dispatch-conf
+#doas emerge --ask --depclean
+#doas eclean -d distfiles
+#doas eclean-pkg
+
+
